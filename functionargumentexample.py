42838e7466e08eed9e8143384e3a37fed19dc7b4
Jet Lee <gamestar109@live.com>
2019-05-22 17:20:30 -0400

    Create functionargumentexample.py

---
+++ functionargumentexample.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import os
+import sys
+
+# This file contains examples of functions as arguments
+
+# Actual definitions
+
+def say_hello(name):
+    return "Hello " + str(name)
+
+def be_awesome(name):
+    return "Yo " + str(name) + ", together we are the awesomest!"
+
+def greet_bob(greeter_func):
+    print(greeter_func("Bob"))  # Takes a function as an argument, then passes "Bob"
+                                # into that function and prints the results.
+
+# Python will run the following code
+
+print("Starting examples of functions as arguments:")
+greet_bob(say_hello)
+greet_bob(be_awesome)
+print("Finished with example")
