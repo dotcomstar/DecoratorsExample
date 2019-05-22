ee607d80d6f594f5d2be6e59824bc04680eb678a
Jet Lee <gamestar109@live.com>
2019-05-22 17:17:45 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import otherfile as f
+import decorators as d
+
+def main():
+    print("Starting main()\n")
+    say_whee()
+    say_oink()
+    say_hi_to_Jawad()
+    print("Finishing main()")
+
+
+# ~~~~ Function definitions ~~~~
+
+@d.simple_decorator  # This is the same as writing say_whee = d.simple_decorator(say_whee)
+def say_whee():
+    print("WHEE")
+
+@d.print_function_names
+def say_oink():
+    print("OINK")
+
+def say_hi_to_Jawad():
+    print("Hi Jawad")
+
+# ~~~~ Don't worry about this for now ~~~~
+if __name__ == "__main__":
+    main()
